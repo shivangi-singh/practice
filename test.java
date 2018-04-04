e136615bc613a8ad657ed1ab7f0b43a55a592ba4
Shivangi Singh <shivangi.singh@intimetec.com>
2018-04-04 11:26:36 +0530

    1 test

--- test.java
+++ test.java
@@ -1,10 +1,6 @@
-package secondProject;
+public class Test{
+public static void main(String[] args){
 
-public class test {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-System.out.println("hekko world");
-	}
-
+System.out.println("Hello Java");
+}
 }
