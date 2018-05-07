95b0e2b04532cc44b3e4feb07e8f48f6f225e47d
Shivangi Singh <shivangi.singh@intimetec.com>
2018-05-07 18:08:17 +0530

    files added

--- dev.java
+++ dev.java
@@ -1,6 +1,6 @@
 class dev{
 public static void main(String[] args){
-<<<<<<< HEAD
+
 System.out.println("dev project");
 =======
 System.out.println("this is dev project");
@@ -8,6 +8,7 @@ System.out.println("this is dev1 project");
 System.out.println("this is dev2 project");
 System.out.println("this is dev3 project");
 System.out.println("this is dev3 project");
->>>>>>> ecdaddb0df6bf9b847e1475ca4e7a9ca1edc55bb
+System.out.println("this is dev35project");
+
 }
 }
